569b3c07bbabe1a5a5721c012df4b3e86949c43d
Shade Hassan <Shade@MacBook-Pro-8.local>
2022-06-27 20:20:16 -0500

    added style and alert

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ window.addEventListener('load', () => {
 
 		const task = input.value;
 
+        if (!task) {
+            alert ("Please create a task!");
+            return;
+    
+        }
+
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
 
